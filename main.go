a4f035a2ae954762f021a0e73a2bc4d08e38615a
tuanbui-n9 <tuanbui.n9@gmail.com>
2024-07-19 21:58:50 +0700

    chore: Update protobuf files and generate code

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/tuanbui-n9/simplebank/api"
 	db "github.com/tuanbui-n9/simplebank/db/sqlc"
@@ -13,6 +15,7 @@ import (
 	"github.com/tuanbui-n9/simplebank/util"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -27,6 +30,8 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
+
 	runGrpcServer(config, store)
 }
 
@@ -49,6 +54,42 @@ func runGrpcServer(config util.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("cannot start grpc server")
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
+	jsonOpt := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{MarshalOptions: protojson.MarshalOptions{
+		UseProtoNames: true,
+	}, UnmarshalOptions: protojson.UnmarshalOptions{
+		DiscardUnknown: true,
+	}})
+
+	grpcMux := runtime.NewServeMux(jsonOpt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register gateway server", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HttpServerAddress)
+	if err != nil {
+		log.Fatalf("cannot listen to address: %v", err)
+	}
+
+	log.Printf("start HTTP server on %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start http gateway server")
 	}
 }
 
