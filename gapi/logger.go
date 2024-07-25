7c559e42e278ebc0ab1f6452ad109d4e8989118f
tuanbui-n9 <tuanbui.n9@gmail.com>
2024-07-25 18:08:56 +0700

    chore: Improve HTTP request logging middleware and response handling

--- gapi/logger.go
+++ gapi/logger.go
@@ -42,17 +42,43 @@ func GrpcLogger(
 	return resp, err
 }
 
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       []byte
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rec *ResponseRecorder) Write(data []byte) (int, error) {
+	if rec.StatusCode == 0 {
+		rec.StatusCode = http.StatusOK
+	}
+	rec.Body = data
+	return rec.ResponseWriter.Write(data)
+}
+
 func HttpLogger(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
-		handler.ServeHTTP(w, r)
+		rec := &ResponseRecorder{ResponseWriter: w, StatusCode: http.StatusOK}
+		handler.ServeHTTP(rec, r)
 		duration := time.Since(startTime)
 
 		logger := log.Info()
+		if rec.StatusCode != http.StatusOK {
+			logger = log.Error().Bytes("response_body", rec.Body)
+		}
+
 		logger.
 			Str("protocol", "http").
 			Str("method", r.Method).
 			Str("path", r.RequestURI).
+			Int("status_code", rec.StatusCode).
+			Str("status_text", http.StatusText(rec.StatusCode)).
 			Dur("duration", duration).
 			Msg("received a HTTP request")
 	})
