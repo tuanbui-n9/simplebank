5cf4c1f89f6877d78633c31f40a10034404acf35
tuanbui-n9 <tuanbui.n9@gmail.com>
2024-07-26 12:50:18 +0700

    chore: Refactor SQLStore to remove unused code and optimize transaction handling

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -2,9 +2,7 @@ package db
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -25,22 +23,4 @@ func NewStore(db *pgxpool.Pool) Store {
 		Queries: New(db),
 		db:      db,
 	}
-}
-
-func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
-	tx, err := store.db.BeginTx(ctx, pgx.TxOptions{})
-	if err != nil {
-		return err
-	}
-
-	q := New(tx)
-	err = fn(q)
-	if err != nil {
-		if rbErr := tx.Rollback(ctx); rbErr != nil {
-			return fmt.Errorf("tx error: %v, rb error: %v", err, rbErr)
-		}
-		return err
-	}
-
-	return tx.Commit(ctx)
 }
